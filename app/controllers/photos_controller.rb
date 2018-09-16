3aee7ce242f669909cffcd1e7e29f8b006809c3e
fgyter <fgyterr@yandex.ru>
2018-09-16 11:02:06 +0300

    v10 генерэйт фото

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,55 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+
+# действия контроллера 7 - CRUD 
+# Create(Сздание) - new + create
+# Read(Просмотр) - show + index
+# Update(обновление) - edit + update
+# Destroy(удаление) - destroy 
+  def index # просмотр все экземпляры
+    @photos = Photo.all
+  end
+
+
+  def show #просмотр (один экземпляр)
+  end
+
+  def new # создание новой фото
+    @photo = Photo.new
+  end
+
+  def edit # обновление - подгружает форму редакриторования
+  end
+
+
+  def create # создание-добавление новой фото в базу данных
+    @photo = Photo.new(photo_params)
+      if @photo.save
+        redirect_to @photo, notice: 'Photo was successfully created.'
+      else
+        render :new
+      end
+  end
+
+  def update # обновление
+      if @photo.update(photo_params)
+        redirect_to @photo, notice: 'Photo was successfully updated.'
+      else
+        render :edit
+      end
+  end
+
+  def destroy # удаление
+    @photo.destroy
+      redirect_to photos_url, notice: 'Photo was successfully destroyed.' 
+  end
+
+  private
+    def set_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_163941) do
+ActiveRecord::Schema.define(version: 2018_09_15_221337) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
