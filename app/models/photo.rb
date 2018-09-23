7f663e3bd1eec8d2cfcc19fffd6d3415fc758b6c
fgyter <fgyterr@yandex.ru>
2018-09-23 21:03:52 +0300

    v13.1 редактирование фото

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,6 @@ class Photo < ApplicationRecord
 	belongs_to :user
 	#одно фото - одному юзеру
 	
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
