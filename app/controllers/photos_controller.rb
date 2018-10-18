8fda974cc9301cb4c24914fb5c2e53a85e573fa6
Fgyter <fgyterr@yandex.ru>
2018-10-18 12:35:11 +0300

    15.2 settings Stripe

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,6 +9,10 @@ class PhotosController < ApplicationController
 # Destroy(удаление) - destroy 
   def index # просмотр все экземпляры
     @photos = Photo.all
+    ###
+    @amount = 500
+    @description = 'Description of Charge'
+    ###
   end
 
 
@@ -30,6 +34,20 @@ class PhotosController < ApplicationController
       else
         render :new
       end
+      ########
+      amount = 500
+
+    @customer = Stripe::Customer.create(email:  params[:stripeEmail],
+                                       source: params[:stripeToken])
+
+    @charge = Stripe::Charge.create(customer:    @customer.id,
+                                    amount:      amount,
+                                    description: 'Rails Stripe customer',
+                                    currency:    'cad')
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+    ########
   end
 
   def update # обновление
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#landing'
   get 'pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
