76cc482fa52d25bd344d2e60ff86214737d6e4e0
Fgyter <fgyterr@yandex.ru>
2018-10-22 12:10:03 +0300

    v19.1 верстка ApplicationController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
-	
   def landing
   	if user_signed_in?
-  		# хелпер devise (проверяет авторизован ли пользователь?)
   		redirect_to "/photos"
-  		# если авторизован то перенаправляет на страницу фотос
   	end
   end
 
